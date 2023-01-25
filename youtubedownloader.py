7a402e1b26ebce3722c0ace35640b6bf802d107b
Karlosso <mail@schulte-manuel.de>
2023-01-25 22:15:16 +0100

    Deployment_2

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -7,7 +7,8 @@ class YT:
 
     def __init__(self, args):
         self.url = args.url
-        self.format = args.format[0]
+        self.format = args.format
+        self.out = args.out
         self.audio = False
         self.download()
 
@@ -27,7 +28,7 @@ class YT:
 
         # Start Download
         print('[#] Downloading ...')
-        out_file = yt_video.download(output_path=os.curdir)
+        out_file = yt_video.download(output_path=self.out)
         base, ext = os.path.splitext(out_file)
         new_file = base + f'.{self.format}'
         os.rename(out_file, new_file)
@@ -43,8 +44,9 @@ if __name__ == '__main__':
     )
 
     # Parse Arguments
-    parser.add_argument('url', type=str, action='store')
-    parser.add_argument('-f', '--format', choices=['mp4', 'mp3'], help='Format in which your Youtube video will be downloaded', nargs=1, default='mp3', type=str, dest='format')
+    parser.add_argument('url', type=str, action='store', help='Url from the YouTube video')
+    parser.add_argument('-f', '--format', choices=['mp4', 'mp3'], help='Format in which your Youtube video will be downloaded', nargs='?', default='mp3', type=str, dest='format')
+    parser.add_argument('-o', '--out', help='Output path for the downloaded YouTube file', nargs='?', default=os.curdir, type=str, dest='out')
     args = parser.parse_args()
 
     try:
